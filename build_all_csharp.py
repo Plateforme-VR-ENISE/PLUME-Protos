7bfd473023c371f83847b23a222e381c9a55b6f3
Charles JAVERLIAT <charles.javerliat@gmail.com>
2023-11-24 23:15:50 +0100

    Added protogen

--- build_all_csharp.py
+++ build_all_csharp.py
@@ -1,14 +1,12 @@
 import pathlib
-from shutil import which, rmtree
+from shutil import rmtree
 import subprocess
-
-if which('protoc') is None:
-    print("protoc not found. Ensure that you installed and added it to your PATH.")
-    exit()
+from sys import platform
 
 protos = [str(path.relative_to("./").as_posix()) for path in pathlib.Path('./').rglob('*.proto')]
 
 rmtree(pathlib.Path("./generated/csharp"), ignore_errors=True)
 pathlib.Path("./generated/csharp").mkdir(parents=True, exist_ok=True)
 
-proc = subprocess.run(["protoc", "--proto_path=./", "--csharp_out=generated/csharp", "--csharp_opt=file_extension=.g.cs,base_namespace=PLUME", *protos])
+program = "./protogen" if platform in ["linux", "linux2"] else "protogen.exe"
+proc = subprocess.run([program, "--proto_path=./", "--csharp_out=generated/csharp", "--package=PLUME", "**/*.proto"])
