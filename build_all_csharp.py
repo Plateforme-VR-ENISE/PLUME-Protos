2c196a7ff99aac36fb84e0306f8a6ad6d2acc20f
Charles JAVERLIAT <charles.javerliat@gmail.com>
2023-12-22 15:55:14 +0100

    Added back protoc for csharp generation

--- build_all_csharp.py
+++ build_all_csharp.py
@@ -3,10 +3,14 @@ from shutil import rmtree
 import subprocess
 from sys import platform
 
-protos = [str(path.relative_to("./").as_posix()) for path in pathlib.Path('./').rglob('*.proto')]
-
 rmtree(pathlib.Path("./generated/csharp"), ignore_errors=True)
 pathlib.Path("./generated/csharp").mkdir(parents=True, exist_ok=True)
 
-program = "./protogen" if platform in ["linux", "linux2"] else "protogen.exe"
-proc = subprocess.run([program, "--proto_path=./", "--csharp_out=generated/csharp", "--package=PLUME", "**/*.proto"])
+use_protogen = False
+
+if use_protogen:
+    program = "./protogen" if platform in ["linux", "linux2"] else "protogen.exe"
+    proc = subprocess.run([program, "--proto_path=./", "--csharp_out=generated/csharp", "--package=PLUME", "**/*.proto"])
+else:
+    protos = [str(path.relative_to("./").as_posix()) for path in pathlib.Path('./').rglob('*.proto')]
+    proc = subprocess.run(["protoc", "--proto_path=./", "--csharp_out=generated/csharp", "--csharp_opt=file_extension=.g.cs,base_namespace=PLUME", *protos])
